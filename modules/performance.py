cdbd2df04182dc7302e5c35787b0bd5a86909657
JoaoV-Ruiz <ruizbarboza@gmail.com>
2026-03-17 15:45:13 -0300

    Update performance.py

--- modules/performance.py
+++ modules/performance.py
@@ -23,24 +23,23 @@ from styles import apply_styles
 from oauth2client.service_account import ServiceAccountCredentials
 
  # --- 1. CONFIGURAÇÃO DE SEGREDOS E CAMINHOS (PATHLIB) ---
-    ERP_USER = st.secrets["ERP_USER"]
-    ERP_PASS = st.secrets["ERP_PASS"]
+ERP_USER = st.secrets["ERP_USER"]
+ERP_PASS = st.secrets["ERP_PASS"]
     
     # Define a raiz do projeto (sobe dois níveis de modules/encerramentos.py)
-    BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent.parent
 
     # Pega os nomes das pastas dos Secrets e garante que são objetos Path
-    NOME_DOWNLOAD = st.secrets["DOWNLOAD_PATH"].strip("/")
-    NOME_DESTINO = st.secrets["DESTINO_PATH"].strip("/")
+NOME_DOWNLOAD = st.secrets["DOWNLOAD_PATH"].strip("/")
+NOME_DESTINO = st.secrets["DESTINO_PATH"].strip("/")
 
-    DOWNLOAD_FOLDER = BASE_DIR / NOME_DOWNLOAD
-    DESTINO_FOLDER = BASE_DIR / NOME_DESTINO
+DOWNLOAD_FOLDER = BASE_DIR / NOME_DOWNLOAD
+DESTINO_FOLDER = BASE_DIR / NOME_DESTINO
 
     # Cria as pastas no servidor
-    DOWNLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
-    DESTINO_FOLDER.mkdir(parents=True, exist_ok=True)
-
-    URL_ERP = "https://erp.osirnet.com.br/all_solicitations#/"
+DOWNLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
+DESTINO_FOLDER.mkdir(parents=True, exist_ok=True)
+URL_ERP = "https://erp.osirnet.com.br/all_solicitations#/"
 
 
 MAPEAMENTO_TECNICOS = {
