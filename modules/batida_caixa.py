6b20d100fd219aa8ceedb7452927baf789c66ef0
JoaoV-Ruiz <ruizbarboza@gmail.com>
2026-03-24 02:50:06 -0300

    Update Google Sheets credentials key in batida_caixa.py

--- modules/batida_caixa.py
+++ modules/batida_caixa.py
@@ -31,7 +31,7 @@ def render():
     def conectar_google_sheets():
         try:
             # 1. Puxa a string bruta do Secret
-            creds_json = st.secrets["GOOGLE_JSON_CREDENTIALS"]
+            creds_json = st.secrets["GOOGLE_JSON_CREDENTIALS_2"]
             spreadsheet_url = st.secrets["URL_PLANILHA"]
     
             # 2. Converte a string para um dicionário Python
