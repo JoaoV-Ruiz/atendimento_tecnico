20cb0b3238ad7814c361e1a7dc32a70572d49a92
JoaoV-Ruiz <ruizbarboza@gmail.com>
2026-03-25 13:43:51 -0300

    Update styles.py

--- styles.py
+++ styles.py
@@ -35,7 +35,6 @@ def apply_styles():
         div[data-testid="stRadio"] [data-baseweb="radio"] div:first-child,
         div[data-testid="stCheckbox"] [data-baseweb="checkbox"] div:first-child {
             background-color: #161b22 !important;
-            border: 1px solid #30363d !important;
             border-radius: 4px !important; /* Deixa o Radio quadrado/levemente arredondado como o checkbox */
         }
 
