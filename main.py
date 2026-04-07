957e3acf1eeba00866e87af81966f56f835f2dc7
JoaoV-Ruiz <ruizbarboza@gmail.com>
2026-04-07 13:11:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ fuso_br = pytz.timezone('America/Sao_Paulo')
 agora = datetime.now(fuso_br)
 
 # --- 5. GATILHO AUTOMÁTICO ---
-HORA_ALVO = 13
-MIN_ALVO = 10
+HORA_ALVO = 23
+MIN_ALVO = 45
 
 if agora.hour == HORA_ALVO and agora.minute == MIN_ALVO:
     if st.session_state.dia_disparo != agora.day:
