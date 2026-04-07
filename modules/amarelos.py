184b2a915181a52c10dd26f9b61f2bd90f3e4d1e
JoaoV-Ruiz <ruizbarboza@gmail.com>
2026-04-06 21:01:27 -0300

    Update amarelos.py

--- modules/amarelos.py
+++ modules/amarelos.py
@@ -77,7 +77,7 @@ def disparar_automacao():
         driver.find_element(By.NAME, "entrar").click()
         
         wait.until(EC.presence_of_element_located((By.XPATH, "//a[@data-checado]")))
-        time.sleep(5) # Espera o carregamento total das linhas
+        time.sleep(10) # Espera o carregamento total das linhas
         
         links_sucesso = driver.find_elements(By.XPATH, "//a[@data-checado]")
         total_sucesso = len(links_sucesso)
